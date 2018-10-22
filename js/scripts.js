560817aa13648fafab63cf56c69a2d7c497a5299
Daniel Bennett and Rohan Moore <daniel@>
2018-10-22 14:22:57 -0700

    CSS and Javascript work

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,53 @@
+var addToList = function(grocery, groceryList) {
+  if(grocery) {
+    groceryList.push(grocery);
+    groceryList.sort();
+  }
+};
+
+var updateList = function(groceryList) {
+  var upperGrocery = groceryList.map(function(grocery) {
+    return grocery.toUpperCase();
+  });
+  upperGrocery.forEach(function(grocery) {
+    $("#grocery-list").append("<li>" + grocery + "</li>")
+  });
+};
+
 $(document).ready(function() {
   var groceryList = [];
   $("#input-form").submit(function(event) {
     event.preventDefault();
-    groceryList.push($("#input").val().toLowerCase());
-    groceryList.sort();
+    addToList($("#input").val().toLowerCase(), groceryList);
     $("#input").val('');
   });
   $("#full-submit").click(function() {
     event.preventDefault();
-    var upperGrocery = groceryList.map(function(grocery) {
-      return grocery.toUpperCase();
-    });
-    upperGrocery.forEach(function(grocery) {
-      $("ul").append("<li>" + grocery + "</li>")
-    });
+    addToList($("#input").val().toLowerCase(), groceryList);
+    updateList(groceryList);
     $("#hideable-form").hide();
     $("#hidden-things").show();
+    $("#editing").hide();
+  });
+  $("#return").click(function() {
+    event.preventDefault();
+    $("#add-section").show();
+    $("#return").hide();
+    $("#complete").hide();
+    $("#editing").show();
+  });
+  $("#add-form").submit(function(event) {
+    $("#grocery-list").children().remove();
+    addToList($("#add").val().toLowerCase(), groceryList);
+    updateList(groceryList);
+    event.preventDefault();
+    $("#add").val('');
+  });
+  $("#done-button").click(function() {
+    event.preventDefault();
+    $("#add-section").hide();
+    $("#return").show();
+    $("#editing").hide();
+    $("#complete").show();
   });
 });
